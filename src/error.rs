32f3eb3653b9d5524eaab6eb99f92c6897c21630
Zack Burt <zack@codefor.cash>
2021-06-23 13:33:41 -0400

    Following tutorial. Non-functional.

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+    #[error("Amount Overflow")]
+    AmountOverflow,
+    #[error("ExpectedAmountMismatch")]
+    ExpectedAmountMismatch
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
